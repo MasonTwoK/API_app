7bc7d12657f6f8ede7ee27e4ac716a2b841f79e5
Will Hamilton <willhamilton1991@gmail.com>
2023-02-27 00:16:51 +0200

    !E lib imorting

--- decorator.py
+++ decorator.py
@@ -1,18 +1,20 @@
 # Python Decorators education with 
 # YouTube video: https://youtu.be/r7Dtus7N4pI
- 
-def decofunc(inside_func):
+
+
+def some_func(indside_func):
     def wrapper(*args):
-        print("Started")
-        inside_func(*args)
-        print("Ended")
-    
+        print("Start")
+        indside_func(*args)
+        print("End")
+
     return wrapper
 
 
-def func(*args):
-    print(*args)
+@some_func
+def func_print(text):
+    print(text)
 
+# func_print = some_func(func_print)('lol')
 
-func = decofunc(func)
-func("Oh", "what", "a", "wonderful", "world")
+func_print("Kek")
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI  # Here we import FastAPI class from \.venv\Lib\site-package\fastapi SOLVED
 
+# SOLVED
 # Q: What is going on here?
 # A: We create instance of class FastAPI which is named app
 app = FastAPI()
 
+# SOLVED
 # Q: What is @ ?
 # A: @ is decorator. Video which explains it https://youtu.be/r7Dtus7N4pI
 @app.get("/")
